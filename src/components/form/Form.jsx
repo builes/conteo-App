95aa80426944ecef1589775481f3fe2abed33143
Mauricio Builes <rmbuilesm@unal.edu.co>
2023-04-20 14:31:22 -0500

    implemented useForm in Form.tsx

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,20 +1,53 @@
+import { useForm } from '../utils/hooks/useForm';
+
 export const Form = () => {
+	const { code, name, quantity, description, onInputChange, onResetForm } =
+		useForm({ code: '', name: '', quantity: '', description: '' });
+
+	const handleCreateProduct = (e) => {
+		e.preventDefault();
+		console.log(code, name, quantity, description);
+		console.log('object');
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleCreateProduct}>
 			<p>
 				Please, fill in all the fields of the form to be able to create a new
 				product
 			</p>
 
-			<input type='number' name='' placeholder='Enter the product code' />
-			<input type='text' name='' placeholder='Enter the product code' />
-			<input type='number' name='' placeholder='Enter how many products' />
+			<input
+				required
+				type='number'
+				name='code'
+				placeholder='Enter the product code'
+				onChange={onInputChange}
+			/>
+			<input
+				type='text'
+				name='name'
+				placeholder='Enter the product name'
+				onChange={onInputChange}
+				required
+			/>
+			<input
+				type='number'
+				name='quantity'
+				placeholder='Enter how many products'
+				onChange={onInputChange}
+				required
+			/>
 			<textarea
-				name=''
+				name='description'
 				cols='30'
 				rows='10'
 				placeholder='Enter the description'
+				onChange={onInputChange}
+				required
 			></textarea>
+			<button type='submit'>Create New Product</button>
+			{/* <button>Delete all fields</button> */}
 		</form>
 	);
 };
